b8e911df9f5bf339ebb890b2dbb87b0f9e688b45
lezzlebit <greenpixel16@gmail.com>
2025-11-08 21:49:52 -0500

    cool thumbnails????

---
+++ art/art.py
@@ -0,0 +1,57 @@
+from PIL import Image
+import os
+
+
+images = os.listdir("assets/")
+
+works = open("artworks.txt").read()
+allworks = works.split(";;")
+
+allhtml = ""
+
+for i in allworks:
+    worktags = i.split("\n")
+    if len(worktags) > 1:
+        if len(worktags[0]) > 1:
+            readimg = Image.open("assets/" + worktags[0])
+            dimensions = str(readimg.width) + 'x' + str(readimg.height)
+            htmlstring = '<img\nlink="' + worktags[1] + '"\ndisplay="assets/' + worktags[0] + '"\nalt="' + worktags[2] + '"\ndimensions="' + dimensions + '">\n'
+            allhtml = allhtml + htmlstring
+
+site = open("index copy.html").read()
+sections = site.split("<!-- IMAGES -->")
+
+newsite = sections[0] + "<!-- IMAGES -->\n" + allhtml + "<!-- IMAGES -->\n" + sections[2]
+print(newsite)
+
+with open("index.html", "w") as f:
+  f.write(newsite)
+
+
+def is_valid_image_pillow(file_name):
+    try:
+        with Image.open(file_name) as img:
+            img.verify()
+            return True
+    except (IOError, SyntaxError):
+        return False
+
+for i in images:
+
+    if is_valid_image_pillow("assets/" + i):
+        og = Image.open("assets/" + i)
+
+        uppercrop = (og.width - og.height) / 2
+
+        if uppercrop > 0:
+            resize = og.crop((uppercrop,0,og.height+uppercrop, og.height))
+        else:
+            resize = og.crop((0,uppercrop*-1,og.width, og.width-uppercrop))
+        resize = resize.resize((128, 128))
+
+        thumbnail = Image.new("RGBA", (128,128))
+        thumbnail.paste(resize, (0,0))
+        if not os.path.exists("assets/thumbnails"):
+            os.mkdir("assets/thumbnails")
+        i = i.replace(".jpg", ".png").replace(".gif", ".png")
+        thumbnail.save("assets/thumbnails/" + i)
--- art/script.js
+++ art/script.js
@@ -1,22 +1,75 @@
-var imageNodes = document.getElementsByTagName('img');
-for (var i=0; i<imageNodes.length; i++)
-{          
-    imageNodes[i].setAttribute("onclick","swipe();");
-}
-  
-function swipe() {
-  var largeImage = event.srcElement;
-  var url = largeImage.getAttribute("src");
-  var alttext = largeImage.getAttribute("alt");
-  var link = largeImage.getAttribute("link");
-  if (alttext == null) {
-    alttext = "";
+
+var images = document.getElementsByTagName('img'); 
+for (var i = 0; i < images.length; i++) {
+  if (images[i].getAttribute("display") != null) {
+    images[i].src = images[i].getAttribute("display").replace(".gif", ".png").replace("assets/", "assets/thumbnails/").replace(".jpg",".png")
   }
-  console.log(alttext);
-  url = url.replace("feed_thumbnail", "feed_fullsize")
-  url = "/art/viewer.html?img=" + url + "&alt=" + alttext + "&link=" + link;
-  window.open(url, "_blank", "noopener, noreferrer");
 }
+
+  var imageNodes = document.getElementsByTagName('img');
+  for (var i=0; i<imageNodes.length; i++)
+  {
+    if (imageNodes[i].id != "main") {
+      imageNodes[i].setAttribute("onclick","setimage();");
+      imageNodes[i].setAttribute("class", "img");
+    }
+
+  }
+  
+  document.getElementById("show").style.display = "none";
+
+function upscale() {
+  
+  if (window.innerWidth >= 500) {
+    if (document.getElementById('main').style.height == 'auto') {
+      document.getElementById('main').style.cssText = 'height: 80vh; cursor:zoom-in;';
+    } else {
+      document.getElementById('main').style.cssText = 'height: auto; cursor:zoom-out;';
+    }
+  } else {
+    window.open(document.getElementById('main').getAttribute("src"), "_blank", "noopener, noreferrer");
+  }
+
+
+}
+
+function setimage() {
+
+document.getElementById("show").style.display = "";
+
+  var largeImage = event.srcElement;
+
+  window.scrollTo(0, 0);
+  var imageToSet = document.getElementById("main");
+
+  var height = largeImage.naturalHeight;
+
+  var slink = document.getElementById("link");
+  var linkText = largeImage.getAttribute("link").replace("https://","");
+  link.href = largeImage.getAttribute("link");
+  if (linkText.length > 35) {
+    link.innerText = linkText.substring(0, 34) + "...";
+  } else {
+      link.innerText = linkText;
+  }
+
+
+  imageToSet.style.cssText = 'height: 80vh; width: auto; cursor:zoom-in;';
+
+  if (window.innerWidth < 500) {
+    imageToSet.style.cssText = 'height: auto; cursor:default;';
+  }
+
+  var src = largeImage.getAttribute("display");
+  imageToSet.setAttribute("src", src);
+  var alt = largeImage.getAttribute("alt");
+  imageToSet.setAttribute("alt", alt);
+  document.getElementById("maintext").innerText = largeImage.getAttribute("alt");
+  var width = imageToSet.naturalWidth;
+  var height = imageToSet.naturalHeight;
+  document.getElementById("resolution").innerText = largeImage.getAttribute("dimensions") + "\n(Click image to view original size)";
+}
+
 
 function newWindow() {
   top.location.href = location.href;
