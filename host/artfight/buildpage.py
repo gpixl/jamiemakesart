5a50363ddcf5ca1a65cdee8faa79284d8b6f8a86
lezzlebit (laptop) <greenpixel16@gmail.com>
2026-07-25 16:59:32 -0400

    artfight mirror

---
+++ host/artfight/buildpage.py
@@ -0,0 +1,13 @@
+
+template = open("template.html").read().split("//")
+
+built = ""
+for i in template:
+	if ".html" in i:
+		insert = open(i).read()
+		built += insert
+	else:
+		built += i
+
+with open("index.html", "w") as f:
+  f.write(built + "\n")
